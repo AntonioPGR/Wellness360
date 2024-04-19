f416354e44f86bce0d0fffc3b92b9c2966c7e487
AntonioPGR <antoninhopgr@gmail.com>
2024-04-19 14:55:11 -0300

    build(backend - nutrition): changing to SQL database schema

--- Backend/nutrition/src/main/java/com/wellness360/nutrition/food/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wellness360.nutrition.food;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodRepository extends MongoRepository<FoodDocument, String> {
-
-}
