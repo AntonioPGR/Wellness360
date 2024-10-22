3612ee13ab964e3a95f06c9c3e3e6a91f23cec3e
AntonioPGR <antoninhopgr@gmail.com>
2024-10-22 19:56:07 -0300

    .

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import Header from "components/Header";
+
+export default function App() {
+  return <div className="App">
+    <Header />
+  </div>
+}
+
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import LOGO from "images/logo_short.svg"
+
+export default function Header(){
+  return <header>
+    <img src={LOGO} alt=""/>
+  </header>
+}
